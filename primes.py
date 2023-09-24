443b90a14c84092810c8aeef6ee86286a9251c32
Furkanvat <117448902+Furkanvat@users.noreply.github.com>
2023-09-24 18:41:44 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -2,5 +2,23 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError
+    
     list = []
+    counter = 2
+    def isPrime(n):
+        if n == 1:
+            return True
+        for i in range(2,n):
+            if (n % i) == 0:
+                return False
+        
+        return True
+            
+    while len(list) < number_of_primes:
+        if isPrime(counter):
+            list.append(counter)
+        counter += 1
+
     return list
